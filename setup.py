95548988a660d036991057caf4261334e44ab8f1
Leopold Franz <leopold.franz@gmx.ch>
2021-12-27 13:55:43 +0100

    Change the setup file to directly also install the correct dependencies.

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-exec(open('pytorch3dunet/__version__.py').read())
-setup(
-    name="pytorch3dunet",
-    packages=find_packages(exclude=["tests"]),
-    version=__version__,
-    author="Adrian Wolny, Lorenzo Cerrone",
-    url="https://github.com/wolny/pytorch-3dunet",
-    license="MIT",
-    python_requires='>=3.7'
-)
+setup()
